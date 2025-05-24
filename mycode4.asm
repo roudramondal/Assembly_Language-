18e57edab1197b5a2532b7419707b9289d811b5f
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-24 12:37:17 +0600

    Update mycode4.asm

--- mycode4.asm
+++ mycode4.asm
@@ -1,23 +1,23 @@
 .model small
-.stack 100h 
+.stack 100h
 
-.data
-msg1 db 10,13,'enter value= $'
-msg2 db 10,13,'result = $'
+.data 
+msg1 db 10,13,'Enter the number = $'
+msg2 db 10,13,'Result = $'
 
-.code
-main proc
-    mov ax,@data
+.code 
+main proc 
+    mov ax,@data 
     mov ds,ax
     
-    mov ah,9
+    mov ah,9H
     lea dx,msg1
     int 21h
     
     mov ah,1
     int 21h
     
-    mov bl,al
+    mov bl,AL
     
     mov ah,9
     lea dx,msg1
@@ -28,12 +28,17 @@ main proc
     
     mov cl,al
     
+    SUB BL, 30H
+    SUB CL, 30H  
+    
     add cl,bl
+    
+    ADD CL, 30H
     
     mov ah,9
     lea dx,msg2
     int 21h
     
-    mov ah,2
-    mov dl,cl
-    int 21h
+    mov ah,2  
+    mov dl,cl 
+    int 21H
