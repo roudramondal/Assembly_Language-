e81796ace26d1a094d85897c3286721007dd9f0d
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-06-20 19:04:31 +0600

    Basic rule to take input ,newline and also output

---
+++ input_output_newline.asm
@@ -0,0 +1,73 @@
+.MODEL SMALL
+.STACK 100H
+
+.DATA 
+MSG1 DB "ENTER NUMBER1= $"
+MSG2 DB "ENTER NUMBER2= $"
+MSG3 DB "VALUE1= $"
+MSG4 DB "VALUE2= $"
+
+.CODE
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    MOV AH,9H
+    LEA DX,MSG1
+    INT 21H 
+    
+    MOV AH,1H
+    INT 21H
+    
+    MOV BL,AL
+    
+    MOV AH,2H
+    MOV DL,0DH
+    INT 21H
+    
+    MOV AH,2H
+    MOV DL,0AH
+    INT 21H  
+    
+    MOV AH,9H
+    LEA DX,MSG3
+    INT 21H
+    
+    MOV AH,2H
+    MOV DL,BL
+    INT 21H   
+    
+    MOV AH,2H
+    MOV DL,0DH 
+    INT 21H
+    
+    MOV AH,2H
+    MOV DL,0AH
+    INT 21H
+    
+    MOV AH,9H
+    LEA DX,MSG2
+    INT 21H  
+    
+    
+    MOV AH,1H
+    INT 21H
+    
+    MOV CL,AL
+    
+    MOV AH,2H
+    MOV DL,0DH
+    INT 21H
+    
+    MOV AH,2H
+    MOV DL,0AH
+    INT 21H
+    
+    MOV AH,9H
+    LEA DX,MSG4
+    INT 21H
+    
+    MOV AH,2H
+    MOV DL,CL
+    INT 21H
+    
