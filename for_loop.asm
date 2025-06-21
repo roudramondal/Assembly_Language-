da12b26b6fe062a2da639d9d278e8a0586293d33
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-06-21 22:30:30 +0600

    Update for_loop.asm

--- for_loop.asm
+++ for_loop.asm
@@ -1,25 +1,29 @@
 .MODEL SMALL
 .STACK 100H
 .DATA
- MSG DB 13,10,'BANGLADESH $'
- .CODE
- MAIN PROC
+M1 DB 10,13,'ROUDRA $'
+
+.CODE
+MAIN PROC
     MOV AX,@DATA
     MOV DS,AX
     
-    MOV AH,09H
-    LEA DX,MSG
-    INT 21H
-    
     MOV CX,0
+
     FOR_LOOP:
     CMP CX,4
     JGE EXIT
+
+    MOV AH,9H
+    LEA DX,M1
+    INT 21H
     
     INC CX
-     JMP FOR_LOOP
- EXIT:
-     MOV AH,4CH
-     INT 21H
-   MAIN ENDP
+    JMP FOR_LOOP
+    
+    EXIT:
+    MOV AH,4CH
+    INT 21H
+    
+    MAIN ENDP
  END MAIN
