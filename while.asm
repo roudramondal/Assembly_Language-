6fa471f6f531c25b83d2e146ccc847f8e70391d7
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-06-21 18:13:35 +0600

    Update while.asm

--- while.asm
+++ while.asm
@@ -25,6 +25,54 @@ WHILE_LOOP:
     INT 21H 
  MAIN ENDP   
  END MAIN
+
+
+
+; New approch 
+
+
+.MODEL SMALL
+.STACK 100H
+
+.DATA
+
+M1 DB 10,13,'ENTER NUMBER= $'
+M2 DB 10,13,'ROUDRA $'
+
+.CODE
+
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
     
+    MOV AH,9H
+    LEA DX,M1
+    INT 21H
+    
+    MOV AH,1H
+    INT 21H
+    
+    SUB AL,30H
+    MOV BL,AL
+    
+    MOV CL,0
+    WHILE_LOOP:
+      
+     CMP CL,BL
+     JGE EXIT
+     
+     MOV AH,9H
+     LEA DX,M2
+     INT 21H
+     
+     INC CL
+     JMP WHILE_LOOP
+     
+     EXIT:
+     MOV AH,4CH
+     INT 21H
+     
+     MAIN ENDP
+   END MAIN
    
 
